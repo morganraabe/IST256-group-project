f59305cb0f9f9ec1f15f5e994a2d188ca542e3d4
ajg6549 <71949956+ajg6549@users.noreply.github.com>
2022-11-04 11:11:22 -0400

    Update content.js

--- public/javascripts/content.js
+++ public/javascripts/content.js
@@ -1,11 +1,13 @@
 function loadData(PersonalData){
-
+    console.log(PersonalData);
     //dumps onto webpage
     //var div =$('<div></div>').append(JSON.stringify(tripInfo));
         $("#content").empty();
-        const span=$("<span></span>").append("username: " + PersonalData[0].username);
-        const paragraph=$("<p></p>").append(person[0].username).append(span);
-        $("#content").append(div);
+        for(let i = 0; i < PersonalData.length; i++){
+            const oneUser = PersonalData[i];
+            const span=$("<span></span>").append("username: " + PersonalData[0].username);
+            const paragraph=$("<p></p>").append(person[0].username).append(span);
+            $("#content").append(div);
     
 }
 
@@ -17,4 +19,14 @@ function setData(){
     const name=$("#updateProfile")[0].value;
 
     $.post("/setProfile", {id,username}, loadData);
+    
+//the first setData was not working for me so i made this one(below), it wasnt working also but it was giving me an error
+//so i think one of the variables is bad
+
+*/function setData(){
+    const bio = $('#updateBio')[0].value;
+    const username = $('#updateUsername')[0].value;
+    $.post("/setProfile", {bio,username}, loadData);
+}/* 
+    
 }
