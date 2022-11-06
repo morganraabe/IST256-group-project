10175d116b972a0f52356fd213dbcc26668a6a86
morganraabe <morganraabe@gmail.com>
2022-11-06 18:06:41 -0500

    updated setData functions

--- public/javascripts/content.js
+++ public/javascripts/content.js
@@ -4,38 +4,40 @@ function loadData(PersonalData){
     //var div =$('<div></div>').append(JSON.stringify(tripInfo));
         $("#content").empty();
         for(let i = 0; i < PersonalData.length; i++){
-            const oneUser = PersonalData[i];
+            const person = PersonalData[i];
             const span=$("<span></span>").append("username: " + PersonalData[0].username);
             const paragraph=$("<p></p>").append(person[0].username).append(span);
             $("#content").append(div);
     
-}
+}}
 
 function getInfo(){
     $.get("/getList", loadData);
 }
 
+//currently gives 500 error, need to connect to database
 function setData(){
-    const name=$("#updateProfile")[0].value;
+    const id=$("#loadID")[0].value;
+    const address=$("#addAddress")[0].value;
 
-    $.post("/setProfile", {id,username}, loadData);
-    
+    $.post("/setProfile", {id,address}, loadData);
+}
 //the first setData was not working for me so i made this one(below), it wasnt working also but it was giving me an error
 //so i think one of the variables is bad
 
-function setData(){
-    const ID =  $('#updateID')[0].value;
-    const bio = $('#updateBio')[0].value;
-    const username = $('#updateUsername')[0].value;
-    $.post("/setProfile", {bio,username}, loadData);
-}
+// function setData(){
+//     const ID =  $('#updateID')[0].value;
+//     const bio = $('#updateBio')[0].value;
+//     const username = $('#updateUsername')[0].value;
+//     $.post("/setProfile", {ID,bio,username}, loadData);
+// }
 
 
-function setAddress(){
-    const ID = $('#updateID')[0].value;
-    const location = $('#addAddress')[0].value;
-    $.post("/setAddress", {ID,location}, loadData);
-}
+// function setAddress(){
+//     const ID = $('#updateID')[0].value;
+//     const location = $('#addAddress')[0].value;
+//     $.post("/setAddress", {ID,location}, loadData);
+// }
 
 function deleteInfo(){
     const ID = $('#updateID')[0].value;
@@ -43,4 +45,4 @@ function deleteInfo(){
     $.post("/setAddress", {ID,location}, loadData);
 }
     
-}
+
